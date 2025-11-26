40dcc032cd8642cc7b05c710e21b13c2b6b595fb
nhan2 <neil_han@intuit.com>
2025-11-26 12:46:35 -0800

    sport timer, Farnsworth cw, life in weeks

--- Life/src/App.svelte
+++ Life/src/App.svelte
@@ -63,7 +63,9 @@
   <div class="years-container">
     {#each gridData.years as year}
       <div class="year-row" title="Year {year.year} (Age {year.age})">
-        <span class="year-label">{year.age % 5 === 0 ? year.age : ''}</span>
+        <div class="year-label">
+          {year.age % 5 === 0 ? year.age : '\u00A0'}
+        </div>
         <div class="weeks-grid">
           {#each year.weeks as week}
             <div 
