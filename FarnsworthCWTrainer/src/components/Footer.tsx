b13552297688589aa9020f2a9befd261a132cd06
nhan2 <neil_han@intuit.com>
2025-11-26 16:19:26 -0800

    sport timer, Farnsworth cw, life in weeks

--- FarnsworthCWTrainer/src/components/Footer.tsx
+++ FarnsworthCWTrainer/src/components/Footer.tsx
@@ -1,6 +1,10 @@
-export const Footer = () => {
+import React from 'react';
+
+export const Footer = (): React.ReactElement => {
     return (
         <div className="footer">
+            This trainer borrows its core idea and portions of the original code
+            from the excellent work by Seth Morabito.<br/>
             Copyright &copy; 2019, Seth Morabito &lt;web@loomcom.com&gt;.<br/>
             Licensed under <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">AGPL 3</a>.
         </div>
